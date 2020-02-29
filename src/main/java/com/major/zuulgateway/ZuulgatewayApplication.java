541da015b545d15f92a23770b7185cec63a14dc9
Apoorv-Saxena-india <apoorvasaxenasrps999@gmail.com>
2020-02-29 13:01:35 +0530

    init commit

---
+++ src/main/java/com/major/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,39 @@
+package com.major.zuulgateway;
+
+import com.major.zuulgateway.zuulFilters.ErrorFilter;
+import com.major.zuulgateway.zuulFilters.PostFilter;
+import com.major.zuulgateway.zuulFilters.PreFilter;
+import com.major.zuulgateway.zuulFilters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableZuulProxy
+@CrossOrigin(origins = "*")
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
